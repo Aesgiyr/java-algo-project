8b7da319813e94dd981e138e20dfbb5a2078f475
Özgür Bilgen <45699938+Aesgiyr@users.noreply.github.com>
2023-08-07 12:04:23 +0300

    Number Compare

---
+++ numberCompare.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class numberCompare {
+
+    public static void main(String[] args) {
+        int winner;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number to compare");
+        int a = input.nextInt();
+        System.out.println("Girilen sayı: "+ a);
+        System.out.println("Enter the second number to compare");
+        int b = input.nextInt();
+        System.out.println("Girilen sayı: "+ b);
+        System.out.println("Enter the third number to compare");
+        int c = input.nextInt();
+        System.out.println("Girilen sayı: "+ c);
+        if (a>b){
+            winner=a;
+        }else{
+            winner=b;
+        }
+        if (winner>c){
+            System.out.println("The largest number entered: "+winner);
+        }else{
+            System.out.println("The largest number entered: "+c);
+        }
+
+    }
+}
